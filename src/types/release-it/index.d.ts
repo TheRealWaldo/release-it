9ee1427bd2cf2291d30f89bab16aa69a10c52768
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-08-06 16:17:59 -0600

    fix: set property requiredUpstream of undefined

--- src/types/release-it/index.d.ts
+++ src/types/release-it/index.d.ts
@@ -1,15 +1,15 @@
 declare module 'release-it' {
   interface options {
-    increment: boolean;
-    git: {
-      requireCleanWorkingDir: boolean;
-      requireUpstream: boolean;
-      tag: boolean;
-      tagName: string;
-      tagAnnotation: string;
-      commit: boolean;
-      commitMessage: string;
-      push: boolean;
+    increment?: boolean;
+    git?: {
+      requireCleanWorkingDir?: boolean;
+      requireUpstream?: boolean;
+      tag?: boolean;
+      tagName?: string;
+      tagAnnotation?: string;
+      commit?: boolean;
+      commitMessage?: string;
+      push?: boolean;
     };
   }
   function runTasks(
